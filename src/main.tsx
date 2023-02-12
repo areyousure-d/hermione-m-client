245aaf3d7c40e60eb0e858c4da24f9aa56779dbd
areyousure <areyousure92@gmail.com>
2023-02-12 13:08:53 +0300

    setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./app";
+import { worker } from "./mocks/browser";
+
+worker.start();
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
