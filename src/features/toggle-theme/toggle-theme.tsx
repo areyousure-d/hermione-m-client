52997601fda9f1afc100fbefabf5163be2a62c2f
areyousure <areyousure92@gmail.com>
2023-02-24 21:16:31 +0300

    refactor toggle theme

--- src/features/toggle-theme/toggle-theme.tsx
+++ src/features/toggle-theme/toggle-theme.tsx
@@ -1,15 +1,40 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import {
+  ActionIcon,
+  Group,
+  useMantineColorScheme,
+  useMantineTheme,
+} from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons";
 
 export const ToggleTheme = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const isDarkTheme = colorScheme === "dark";
+  const theme = useMantineTheme();
 
   const onClick = () => toggleColorScheme();
 
   return (
-    <ActionIcon variant="light" onClick={onClick}>
-      {isDarkTheme ? <IconSun /> : <IconMoon />}
-    </ActionIcon>
+    <Group position="center">
+      <ActionIcon
+        onClick={onClick}
+        size="lg"
+        sx={(theme) => ({
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[4]
+              : theme.colors.gray[1],
+          color:
+            theme.colorScheme === "dark"
+              ? theme.colors.yellow[4]
+              : theme.colors.dark[6],
+        })}
+      >
+        {isDarkTheme ? (
+          <IconSun size={18} fill={theme.colors.yellow[4]} />
+        ) : (
+          <IconMoon size={18} fill={theme.colors.dark[4]} />
+        )}
+      </ActionIcon>
+    </Group>
   );
 };
