2a579864164e858b391d7fd7fc799ab160eb933c
areyousure <areyousure92@gmail.com>
2023-05-29 19:37:03 +0300

    chore(eslint): add eslint and path aliases

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,23 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+const { configure, presets } = require("eslint-kit");
+
+module.exports = configure({
+  presets: [
+    presets.node(),
+    presets.imports({
+      sort: {
+        newline: true,
+      },
+      alias: {
+        root: "./src",
+        "@": "./",
+      },
+    }),
+    presets.typescript({
+      root: "./",
+      tsconfig: "tsconfig.json",
+    }),
+    presets.react({
+      version: "18.0",
+    }),
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [react()],
-})
+
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+  },
+});
