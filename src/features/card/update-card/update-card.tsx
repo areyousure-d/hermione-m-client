281886a4e28860099b8a2bb88a55667974d9fc44
areyousure <areyousure92@gmail.com>
2023-06-19 17:04:19 +0300

    fix(card): fix update card

--- src/features/card/update-card/update-card.tsx
+++ src/features/card/update-card/update-card.tsx
@@ -1,5 +1,4 @@
 import { useUnit } from "effector-react";
-import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { Card, CardEditForm, cardListQuery } from "@/entities/card";
@@ -7,22 +6,10 @@ import { Card, CardEditForm, cardListQuery } from "@/entities/card";
 import { updateCardMutation } from "./model";
 
 export const UpdateCard = () => {
-  const [card, setCard] = useState<Card | null>(null);
-
   const navigate = useNavigate();
   const { deckId, cardId } = useParams() as { deckId: string; cardId: string };
   const { start: updateCard } = useUnit(updateCardMutation);
   const [cardList] = useUnit([cardListQuery.$data]);
-
-  useEffect(() => {
-    if (cardList) {
-      const card = cardList.find((card) => card.id === Number(cardId));
-
-      if (card) {
-        setCard(card);
-      }
-    }
-  }, [cardList, cardId]);
 
   const submit = (formValues: Pick<Card, "front" | "back">) => {
     updateCard({
@@ -33,6 +20,10 @@ export const UpdateCard = () => {
 
     navigate(`/decks/${deckId}/cards`);
   };
+
+  const card = cardList
+    ? cardList.find((card) => card.id === Number(cardId))
+    : null;
 
   return <CardEditForm submit={submit} card={card} />;
 };
