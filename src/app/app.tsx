1e7b592443d84c7565cbb30410fa3f5af5e77a56
areyousure <areyousure92@gmail.com>
2023-06-28 16:07:21 +0300

    fix(auth): put token in localStorage

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
+
 import { Routing } from "@/pages";
+import { checkTokenInLs } from "@/shared/auth/token";
 import { BasicLayout } from "@/shared/ui/basic-layout";
 import { Header } from "@/widgets/header";
 
 import { Providers } from "./providers";
 
 export const App = () => {
+  useEffect(() => {
+    checkTokenInLs();
+  }, []);
+
   return (
     <Providers>
       <BasicLayout header={<Header />}>
--- src/shared/auth/token.ts
+++ src/shared/auth/token.ts
@@ -8,3 +8,25 @@ export const $token = createStore<string | null>(null);
 export const $isAuthorized = $token.map(Boolean);
 
 $token.on(tokenReceived, (_, token) => token).reset(tokenErased);
+
+const tokenKey = "hermione-m-token";
+
+tokenReceived.watch((token) => {
+  localStorage.setItem(tokenKey, token);
+});
+
+tokenErased.watch(() => {
+  localStorage.removeItem(tokenKey);
+});
+
+const tokenReceivedFromLs = createEvent<string>();
+
+$token.on(tokenReceivedFromLs, (_, token) => token);
+
+export const checkTokenInLs = () => {
+  const token = localStorage.getItem(tokenKey);
+
+  if (token) {
+    tokenReceivedFromLs(token);
+  }
+};
