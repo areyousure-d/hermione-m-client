4c11eb2a467cef4f244db82496a50f9e84b2da92
areyousure <areyousure92@gmail.com>
2023-06-28 16:13:47 +0300

    feat(cards): add back button to create card page

--- src/pages/create-card/create-card.tsx
+++ src/pages/create-card/create-card.tsx
@@ -1,10 +1,25 @@
 import { Container, Title } from "@mantine/core";
+import { useParams } from "react-router-dom";
 
 import { CreateCard } from "@/features/card/create-card";
+import { ButtonLink } from "@/shared/ui/button-link";
+import { Icon } from "@/shared/ui/icon";
 
 export const CreateCardPage = () => {
+  const { deckId } = useParams() as { deckId: string };
+
   return (
     <Container>
+      <ButtonLink
+        to={`/decks/${deckId}`}
+        mb="lg"
+        leftIcon={
+          <Icon type="common" name="arrow-left" width={20} height={20} />
+        }
+      >
+        Back to deck
+      </ButtonLink>
+
       <Title order={2} mb="lg">
         Create Card
       </Title>
