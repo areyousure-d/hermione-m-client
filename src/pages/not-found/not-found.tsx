1fafa9f1cede317273bf71e74be3eb297e45d0c2
areyousure <areyousure92@gmail.com>
2023-06-29 16:21:07 +0300

    fix(pages): add description for not found page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,3 +1,23 @@
+import { Box, Center, Container, Group, Text, Title } from "@mantine/core";
+
+import { ButtonLink } from "@/shared/ui/button-link";
+
 export const NotFoundPage = () => {
-  return <div>not found page</div>;
+  return (
+    <Container>
+      <Center mt="15%">
+        <Box>
+          <Title align="center" mb="md">
+            404
+          </Title>
+          <Text mb="xl" align="center">
+            This page could not be found
+          </Text>
+          <Group position="center">
+            <ButtonLink to="/">Home page</ButtonLink>
+          </Group>
+        </Box>
+      </Center>
+    </Container>
+  );
 };
